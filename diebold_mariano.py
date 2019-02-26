c747d98c963dbe1e0ff35057dd6f27727f4d0d60
Viktor Pekar <v.pekar@gmail.com>
2019-02-26 11:21:18 +0000

    rmse in diebold_mariano.py

--- diebold_mariano.py
+++ diebold_mariano.py
@@ -4,8 +4,11 @@ Diebold-Mariano test
 The script require dm_test.py from [here](https://github.com/johntwk/Diebold-Mariano-Test) to be placed n nthe same folder
 """
 
+import numpy as np
 import importlib
 import logging
+
+from sklearn.metrics import mean_squared_error
 from learner_configs import ConfigLSTM
 from dm_test import dm_test
 from utils import run_config, prepare_data
@@ -16,7 +19,7 @@ HORIZON = 1
 LOGGER = logging.getLogger('dmtest')
 
 
-def get_forecasts(learner, c, pc):
+def get_forecasts(learner, dataset_name, prepr_name, c, pc):
 
     # create config instances
     LearnerConfig = getattr(importlib.import_module("learner_configs"),
@@ -29,15 +32,19 @@ def get_forecasts(learner, c, pc):
     learner_config = LearnerConfig(c, pc)
     result_test = run_config([data, learner_config, 'test'])
     LOGGER.debug(f"Ran config: {c}")
+    actual = data.testYref
+    pred = data.revert(result_test.yhat_oos, "test")
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    print(f"{learner}, {dataset_name}, {prepr_name}, RMSE: {rmse:.3f}")
 
-    return data.testYref.flatten(), result_test.yhat_oos.flatten()
+    return actual.flatten(), pred.flatten()
 
 
 run1 = ('RFR', 'autoreg', 'autoreg', {'n_estimators': 50, 'max_features': 0.6, 'max_depth': 50, 'min_samples_split': 5, 'min_samples_leaf': 5, 'max_leaf_nodes': None}, {'data_file': 'data/3day.1000.all.facebook.csv', 'date_format': '%d/%m/%Y', 'test_split': 0.2, 'difference': 1, 'deseason': 0, 'seasonal_period': 7, 'horizon': HORIZON, 'feature_selection': 0, 'rfe_step': 0, 'use_exog': 0, 'lags': 7, 'scale_range': [0, 1], 'n_jobs': 2, 'random_state': 7})
-run2 = ('RFR', 'fb100', 'mcgc', {'n_estimators': 500, 'max_features': 0.6, 'max_depth': 10, 'min_samples_split': 5, 'min_samples_leaf': 5, 'max_leaf_nodes': None}, {'data_file': 'data/3day.majorclust-gc.facebook.csv', 'date_format': '%d/%m/%Y', 'test_split': 0.2, 'difference': 1, 'deseason': 0, 'seasonal_period': 7, 'horizon': 1, 'feature_selection': 0, 'rfe_step': 0, 'use_exog': 1, 'lags': 7, 'scale_range': [0, 1], 'n_jobs': 2, 'random_state': 7})
+run2 = ('RFR', 'tw100', 'mcgc', {'n_estimators': 25, 'max_features': 0.8, 'max_depth': 5, 'min_samples_split': 5, 'min_samples_leaf': 5, 'max_leaf_nodes': None}, {'data_file': 'data/3day.majorclust-gc.twitter.short.csv', 'date_format': '%d/%m/%Y', 'test_split': 0.2, 'difference': 1, 'deseason': 0, 'seasonal_period': 7, 'horizon': 1, 'feature_selection': 0, 'rfe_step': 0, 'use_exog': 1, 'lags': 7, 'scale_range': [0, 1], 'n_jobs': 2, 'random_state': 7})
 
-actual1, pred1 = get_forecasts(run1[0], run1[3], run1[4])
-actual2, pred2 = get_forecasts(run2[0], run2[3], run2[4])
+actual1, pred1 = get_forecasts(*run1)
+actual2, pred2 = get_forecasts(*run2)
 
 pval = dm_test(actual1, pred1, pred2, h=HORIZON, crit="MSE").p_value
 
